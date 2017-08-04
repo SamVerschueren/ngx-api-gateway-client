e064ba4238075115c040730c4783aa04df34b324
Sam Verschueren <sam.verschueren@gmail.com>
2017-08-04 14:23:01 +0200

    fix restoring authentication tokens

--- src/lib/aws-http.service.ts
+++ src/lib/aws-http.service.ts
@@ -31,20 +31,19 @@ export class AWSHttpService {
 		const data = JSON.parse(window.localStorage.getItem(IDENTITY_DATA));
 
 		if (params && data) {
+			// Restore previous credentials
 			data.Credentials.Expiration = new Date(data.Credentials.Expiration);
 
-			if (isExpired(data.Credentials.Expiration)) {
-				// If the token is expired, do not restore them but just set them and the refresh mechanism will do the hard work the next time.
-				const cognitoCredentials = new AWS.CognitoIdentityCredentials(params);
-				cognitoCredentials.data = data;
-				cognitoCredentials.expireTime = data.Credentials.Expiration;
-				cognitoCredentials.expired = true;
+			const cognitoCredentials = new AWS.CognitoIdentityCredentials(params);
+			cognitoCredentials.identityId = data.IdentityId;
+			cognitoCredentials.accessKeyId = data.Credentials.AccessKeyId;
+			cognitoCredentials.sessionToken = data.Credentials.SessionToken;
+			cognitoCredentials.secretAccessKey = data.Credentials.SecretKey;
+			cognitoCredentials.data = data;
+			cognitoCredentials.expireTime = data.Credentials.Expiration;
+			cognitoCredentials.expired = isExpired(data.Credentials.Expiration);
 
-				AWS.config.credentials = cognitoCredentials;
-			} else {
-				// If the credentials are not yet expired, restore them.
-				this.restoreCredentials(params, data);
-			}
+			AWS.config.credentials = cognitoCredentials;
 		}
 	}
 
@@ -84,9 +83,6 @@ export class AWSHttpService {
 				const cognitoCredentials = new AWS.CognitoIdentityCredentials(params);
 				cognitoCredentials.data = data;
 
-				window.localStorage.setItem(IDENTITY_PARAMS, JSON.stringify(params));
-				window.localStorage.setItem(IDENTITY_DATA, JSON.stringify(data));
-
 				this.loadCredentials(params, data)
 					.then(() => {
 						observer.next();
@@ -106,22 +102,6 @@ export class AWSHttpService {
 		AWS.config.credentials = undefined;
 	}
 
-	private restoreCredentials(params: any, data: any) {
-		// Pause all requests while restoring the credentials
-		this.paused$.next(true);
-
-		this.loadCredentials(params, data)
-			.catch(err => {
-				// Loading credentials went wrong, so sent a error to all in flight requests and start all over again
-				this.paused$.error(err);
-				this.paused$ = new BehaviorSubject(true);
-			})
-			.then(() => {
-				// Unpause other requests
-				this.paused$.next(false);
-			});
-	}
-
 	private loadCredentials(params: any, data: any) {
 		return new Promise((resolve, reject) => {
 			const cognitoCredentials = new AWS.CognitoIdentityCredentials(params);
@@ -131,6 +111,9 @@ export class AWSHttpService {
 				if (err) {
 					return reject(err);
 				}
+
+				window.localStorage.setItem(IDENTITY_PARAMS, JSON.stringify(params));
+				window.localStorage.setItem(IDENTITY_DATA, JSON.stringify(data));
 
 				AWS.config.credentials = cognitoCredentials;
 
